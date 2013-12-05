4f44ecfbcdc42f9e6a89ecaf3c585e054ef9366c
ChrisProkop <cprokop@gmail.com>
2013-12-05 13:35:54 -0600

    Added Elegant_BeamlineAlpha.sh and Elegant_BeamlineBeta.sh

---
+++ Elegant_BeamlineAlpha.sh
@@ -0,0 +1,11 @@
+<<<<<<< HEAD
+# Requires sddsplot
+# Argument one is the root filename of the simulation.
+# Argument two and argument three are additional arguments for sddsplot
+=======
+# Requires sddsplot
+# Argument one is the root filename of the simulation.
+# Argument two and argument three are additional arguments for sddsplot
+>>>>>>> 114b49eb133b17b49333f96f7269d629e0cf3532
+sddsplot $2 -legend -column=s,alphax -column=s,alphay -graph=line,vary \
+      $1.twi -column=s,Profile -graph=line,type=7 -overlay=xmode=norm,yfact=0.1,yoffset=10 $1.mag -title='' -topline=' ' $3
---
+++ Elegant_BeamlineBeta.sh
@@ -0,0 +1,11 @@
+<<<<<<< HEAD
+# Requires sddsplot
+# Argument one is the root filename of the simulation.
+# Argument two and argument three are additional arguments for sddsplot
+=======
+# Requires sddsplot
+# Argument one is the root filename of the simulation.
+# Argument two and argument three are additional arguments for sddsplot
+>>>>>>> 114b49eb133b17b49333f96f7269d629e0cf3532
+sddsplot $2 -legend -column=s,betax -column=s,betay -graph=line,vary \
+      $1.twi -column=s,Profile -graph=line,type=7 -overlay=xmode=norm,yfact=0.1,yoffset=5 $1.mag -title='' -topline=' ' $3
